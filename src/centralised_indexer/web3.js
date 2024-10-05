5ad519aca4e1a6665525547faf381d2fac7b9737
chirag <sharma.chirag3737@gmail.com>
2024-10-05 14:06:17 +0530

    Refactor interval value for getLogs function

--- src/centralised_indexer/web3.js
+++ src/centralised_indexer/web3.js
@@ -91,7 +91,7 @@ export const getLogs = async (
   fromBlock,
   toBlock
 ) => {
-  const interval = 1000;
+  const interval = 10;
   const provider = contract.provider;
   const allLogs = [];
   for (let i = fromBlock; i < toBlock; i += interval) {
