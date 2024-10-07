b58238fa9e551b8043e146acd445dc4c5125d31d
chirag <sharma.chirag3737@gmail.com>
2024-10-07 15:57:24 +0530

    move to recursion

--- src/centralised_indexer/index.js
+++ src/centralised_indexer/index.js
@@ -17,13 +17,13 @@ const evmBlockScanner = async () => {
     // const canisterId = "bkyz2-fmaaa-aaaaa-qaaaq-cai";
     const canisterId="5qtxp-vyaaa-aaaap-qh5rq-cai";
 
-    const signBackend = createActor(canisterId, {
-      agentOptions: {
-        identity,
-        // host: "http://127.0.0.1:4943",
-        host :"https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/?id=5qtxp-vyaaa-aaaap-qh5rq-cai",
-      },
-    });
+    // const signBackend = createActor(canisterId, {
+    //   agentOptions: {
+    //     identity,
+    //     // host: "http://127.0.0.1:4943",
+    //     host :"https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/?id=5qtxp-vyaaa-aaaap-qh5rq-cai",
+    //   },
+    // });
 
     // console.log("signBackend", signBackend);
     //recheck missed Lock Events on common contract
@@ -53,7 +53,7 @@ const evmBlockScanner = async () => {
       fromBlockNumber,
       toBlockNumber
     );
-    // console.log("Logs got from event: ", logs);
+    console.log("Logs got from event: ", logs);
     let transac;
     let des;
     let amount;
@@ -68,19 +68,19 @@ const evmBlockScanner = async () => {
       transac = log.transactionHash;
     }
 
-    if (!transac || !des || !amount || !dest_chain_id) {
-      console.log("No logs found");
-      return;
+    if (transac && des && amount && dest_chain_id) {
+      const result = await signBackend.verify_trans(
+        transac,
+        des,
+        amount,
+        dest_chain_id
+      );
+      console.log("Signed backend", result.Ok);
     }
-    const result = await signBackend.verify_trans(
-      transac,
-      des,
-      amount,
-      dest_chain_id
-    );
-    console.log("Signed backend", result.Ok);
+  
+   await fs.promises.writeFile("block.txt", toBlockNumber.toString() , "utf-8");
 
-    fs.writeFileSync("block.txt", toBlockNumber.toString(), "utf-8");
+  
 
     // for (const log of logs) {
     //   const parsedLog = contract.interface.parseLog(log);
@@ -98,20 +98,17 @@ const evmBlockScanner = async () => {
     console.log("Starting evmBlockScanner");
     // evmBlockScanner();
     // loggers.logger.error(`🚫 | error in EVM block Scanner`, e.message);
+  } finally {
+    evmBlockScanner();
   }
 };
 
 //run evmBlockScanner as a cron job
 
-cron.schedule("*/10 * * * * *", async () => {
-  console.log("Running EVM Block Scanner");
-  await evmBlockScanner();
-  console.log("To Block Number: ", toBlockNumber);
-});
-
-// evmBlockScanner().then(() => {
-//   console.log("EVM Block Scanner started");
-// }).catch((e) => {
-//   console.log("Error in EVM Block Scanner: ", e);
-
+// cron.schedule("*/10 * * * * *", async () => {
+//   console.log("Running EVM Block Scanner");
+//   await evmBlockScanner();
+//   console.log("To Block Number: ", toBlockNumber);
 // });
+
+evmBlockScanner();
